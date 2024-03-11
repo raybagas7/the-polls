91e2aefd9be3f61b801021e5a624aab0af4b5aa1
raybagas7 <raysam.gas@gmail.com>
2024-03-11 16:12:29 +0700

    feat(components): - make Header and Footer components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script>
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 
+<Header />
 <main>
-  <h1>Hello Pal!</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis accusamus
+    quisquam veniam ex, natus tenetur voluptate. Quidem sint velit perspiciatis
+    et beatae! Dolorum at, asperiores ratione consectetur error dolore labore.
+  </p>
 </main>
+<Footer />
 
 <style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
