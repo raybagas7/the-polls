22d21216e259a7b142f834d2358d01394d20a7b4
raybagas7 <raysam.gas@gmail.com>
2024-03-13 13:22:55 +0700

    feat(Lifecycle Hooks) - try a Lifecycle Hooks from svelte store on onMount and onDestroy - use the fastest way to use/subs and unsubs the store from svelte

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,16 +1,28 @@
 <script>
+  // import { onDestroy, onMount } from 'svelte';
   import PollStore from '../stores/PollStore';
   import PollDetails from './PollDetails.svelte';
+  // IMPORTANT when you want to automatically subs and unsubs the data just use the store inside the component directly
+  // insted of change the polls data
 
-  export let polls = [];
+  // export let polls = [];
 
-  PollStore.subscribe((data) => {
-    polls = data;
-  });
+  // const unsub = PollStore.subscribe((data) => {
+  //   polls = data;
+  // });
+
+  // onMount(() => {
+  //   console.log('component mounted');
+  // });
+
+  // onDestroy(() => {
+  //   console.log('component destroyed');
+  //   unsub();
+  // });
 </script>
 
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  {#each $PollStore as poll (poll.id)}
     <div>
       <PollDetails {poll} on:vote />
     </div>
