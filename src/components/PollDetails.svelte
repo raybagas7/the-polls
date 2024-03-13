c0e179c2bf7a23922ef086aa8f75d3d832e2b167
raybagas7 <raysam.gas@gmail.com>
2024-03-13 13:47:04 +0700

    fix(Button component): - add on:click on button component so it can listen to click event feat(Delete Polls): - make an update store to delete the polls

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,6 +1,7 @@
 <script>
   import PollStore from '../stores/PollStore';
   import Card from '../shared/Card.svelte';
+  import Button from '../shared/Button.svelte';
   export let poll;
 
   // reactive value
@@ -21,6 +22,12 @@
       return copiedPolls;
     });
   };
+
+  const handleDelete = (id) => {
+    PollStore.update((currentPolls) => {
+      return currentPolls.filter((poll) => poll.id != id);
+    });
+  };
 </script>
 
 <Card>
@@ -36,6 +43,10 @@
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
       <div class="percent percent-b" style="width: {percentB}%;"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
+    </div>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="delete">
+      <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
     </div>
   </div>
 </Card>
@@ -76,5 +87,9 @@
   .percent-b {
     border-left: 4px solid #45c469;
     background: rgba(69, 196, 150, 0.2);
+  }
+  .delete {
+    margin-top: 30px;
+    text-align: center;
   }
 </style>
