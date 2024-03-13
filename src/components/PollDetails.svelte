ede46485b14386e3bffdc7c35cb37f970b5f403f
raybagas7 <raysam.gas@gmail.com>
2024-03-13 15:07:31 +0700

    feat(Tweens and animations) - uuse tweens to animate the width of the poll with reactive value when the percentage change

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -2,12 +2,21 @@
   import PollStore from '../stores/PollStore';
   import Card from '../shared/Card.svelte';
   import Button from '../shared/Button.svelte';
+  import { tweened } from 'svelte/motion';
+
   export let poll;
 
   // reactive value
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+
   // handling votes
   const handleVote = (option, id) => {
     PollStore.update((currentPolls) => {
@@ -36,12 +45,12 @@
     <p>Total votes: {totalVotes}</p>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a" style="width: {percentA}%;"></div>
+      <div class="percent percent-a" style="width: {$tweenedA}%;"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%;"></div>
+      <div class="percent percent-b" style="width: {$tweenedB}%;"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
